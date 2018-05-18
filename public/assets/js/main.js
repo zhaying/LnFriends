e3802b4535c9e51e628b5c22fba5c6492be0b98c
ZhAYinG <8896093+zhaying@users.noreply.github.com>
2018-05-18 09:16:42 -0400

    fixing names and  errors in frontend js

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -3,4 +3,9 @@
 $(document).ready( function () {
   console.log("document ready!");
     $('#myDataTable').DataTable();
+    $('#myDashboardTable').DataTable();
+    $('#myRigTable').DataTable();
+    $('#myInvestorTable').DataTable();
+    $('#myWalletTable').DataTable();
+    $('#myMiningPoolTable').DataTable();
 } );
